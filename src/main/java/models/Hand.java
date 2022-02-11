cc4c7e56e73a42ab986c812c5e9af176338204af
ziP3Rz <alvarolpizarro@gmail.com>
2022-02-11 17:09:12 +0100

    Enums implementados (por revisar)

--- src/main/java/models/Hand.java
+++ src/main/java/models/Hand.java
@@ -1,7 +1,35 @@
 package models;
 
 import models.deck.Card;
+import models.deck.Hands;
 
 public class Hand {
-    private Card[] hand = new Card[5];
+
+    private Card[] fullHand = new Card[5];
+    private Hands matchedHand;
+    private Card[] orderedHand;
+
+    public void orderHand(){
+        Card exchanger = fullHand[0];
+        for(int i = 1; i < fullHand.length; i++){
+            if(exchanger.getNum() > fullHand[i].getNum()){
+                fullHand[i-1] = fullHand[i];
+                //fullHand[i];
+            }
+        }
+    }
+
+    //TODO: implementar este método: devolverá -1 en caso de que la mano invocante sea peor que la mano pasada como parámetro, 1 en caso contrario y 0 en caso de que sean iguales (equivalentes en valor).
+    public int compareTo(Hand otherHand){
+        if(this.matchedHand.equals(otherHand.matchedHand)){
+            return 0;
+        }
+        if((this.matchedHand.getValue() == otherHand.matchedHand.getValue())){
+            //if()
+        }
+        if(this.matchedHand.getValue() < otherHand.matchedHand.getValue()){
+            return -1;
+        }
+        return 1;
+    }
 }
--- src/main/java/models/deck/Numbers.java
+++ src/main/java/models/deck/Numbers.java
@@ -1,19 +1,20 @@
 package models.deck;
 
 public enum Numbers {
-    AS (1, "A"),
-    TWO (2, "2"),
-    THREE (3, "3"),
-    FOUR (4, "4"),
-    FIVE (5, "5"),
-    SIX (6, "6"),
-    SEVEN (7, "7"),
-    EIGHT (8, "8"),
-    NINE (9, "9"),
-    TEN (10, "10"),
-    JACK (11, "J"),
-    QUEEN (12, "Q"),
-    KING (13, "K");
+
+    TWO (1, "2"),
+    THREE (2, "3"),
+    FOUR (3, "4"),
+    FIVE (4, "5"),
+    SIX (5, "6"),
+    SEVEN (6, "7"),
+    EIGHT (7, "8"),
+    NINE (8, "9"),
+    TEN (9, "10"),
+    JACK (10, "J"),
+    QUEEN (11, "Q"),
+    KING (12, "K"),
+    AS (13, "A");
 
 
 
